6fbbd7cb0f53ed1c28ce433eb983ed82cfa89447
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-05 14:56:22 +0500

    scrore label style extension is added to the project

---
+++ Slot Machine/Helpers/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  Slot Machine
+//
+//  Created by Elexoft on 05/03/2025.
+//
+
+import SwiftUI
+
+extension Text {
+    func scoreLabelStyle() -> Text {
+        self
+            .foregroundColor(Color.white)
+            .font(.system(size: 10, weight: .bold, design: .rounded))
+    }
+}
