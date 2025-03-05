1250a1e2cba83a29c624a639c27da5af39d21151
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-05 13:01:36 +0500

    Some prjects folders are get done for better undertanding

---
+++ Slot Machine/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Slot Machine
+//
+//  Created by Elexoft on 05/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
