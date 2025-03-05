606d9fc043854ff3fd4bb44b0955f2da3306294c
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-05 12:43:27 +0500

    Initial Commit

---
+++ Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Elexoft on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
